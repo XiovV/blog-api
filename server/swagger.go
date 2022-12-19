bf407f1ef153100ecc88a020671474b97866e3a1
XiovV <xiovv@tutanota.com>
2022-12-19 15:22:41 +0100

    swagger metadata

---
+++ server/swagger.go
@@ -0,0 +1,17 @@
+// Package server defines the blog api.
+//
+// The purpose of this application is to provide an application
+//
+//	Schemes: https
+//	Host: localhost
+//	BasePath: /v1
+//	Version: 0.0.1
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+package server
