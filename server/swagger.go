02b661dd7ebf4c724f676fd5594e43c63d666cd1
XiovV <xiovv@tutanota.com>
2022-12-21 14:37:35 +0100

    added tokenPair model

--- server/swagger.go
+++ server/swagger.go
@@ -2,16 +2,25 @@
 //
 // The purpose of this application is to provide an application
 //
-//	Schemes: https
-//	Host: localhost
-//	BasePath: /v1
-//	Version: 0.0.1
+//		Schemes: https
+//		Host: localhost
+//		BasePath: /v1
+//		Version: 0.0.1
 //
-//	Consumes:
-//	- application/json
+//		Consumes:
+//		- application/json
 //
-//	Produces:
-//	- application/json
+//		Produces:
+//		- application/json
+//
+//	    Security:
+//	    - access_token:
+//
+//	    SecurityDefinitions:
+//	    access_token:
+//	         type: apiKey
+//	         name: KEY
+//	         in: header
 //
 // swagger:meta
 package server
@@ -20,4 +29,12 @@ package server
 type errorResponse struct {
 	// Error response model
 	Error string `json:"error"`
+}
+
+// swagger:model
+type tokenPair struct {
+	// user's access token
+	AccessToken string `json:"access_token"`
+	// user's refresh token
+	RefreshToken string `json:"refresh_token"`
 }
