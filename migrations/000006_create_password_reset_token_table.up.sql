b0b325e1c66b09c9ef138b7d5d4ceb260648d60c
XiovV <xiovv@tutanota.com>
2023-01-05 14:21:51 +0100

    migrations for password reset token table

---
+++ migrations/000006_create_password_reset_token_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS token_blacklist(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    user_id BIGINT NOT NULL,
+    token text NOT NULL,
+    expiry INT NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(user_id)
+        REFERENCES "user"(id)
+        ON DELETE CASCADE
+)
