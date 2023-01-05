fed97186c31003fc21d6bbce6bb9377034176881
XiovV <xiovv@tutanota.com>
2023-01-05 16:46:09 +0100

    methods for password reset tokens

--- pkg/repository/token.go
+++ pkg/repository/token.go
@@ -1,12 +1,12 @@
 package repository
 
-type Token struct {
+type RefreshToken struct {
 	ID     int
 	UserID int `db:"user_id"`
 	Token  string
 }
 
-func (r *UserRepository) InsertRefreshToken(token Token) error {
+func (r *UserRepository) InsertRefreshToken(token RefreshToken) error {
 	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
 	defer cancel()
 
@@ -15,7 +15,7 @@ func (r *UserRepository) InsertRefreshToken(token Token) error {
 }
 
 func (r *UserRepository) IsRefreshTokenBlacklisted(userId int, token string) (bool, error) {
-	var tok Token
+	var tok RefreshToken
 
 	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
 	defer cancel()
@@ -26,4 +26,49 @@ func (r *UserRepository) IsRefreshTokenBlacklisted(userId int, token string) (bo
 	}
 
 	return true, nil
+}
+
+type PasswordResetToken struct {
+	ID     int
+	UserID int `db:"user_id"`
+	Token  string
+	Expiry int64
+}
+
+func (r *UserRepository) InsertPasswordResetToken(token PasswordResetToken) error {
+	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
+	defer cancel()
+
+	_, err := r.db.ExecContext(ctx, "INSERT INTO password_reset_token (user_id, token, expiry) VALUES ($1, $2, $3)", token.UserID, token.Token, token.Expiry)
+	return r.handleError(err)
+}
+
+func (r *UserRepository) GetPasswordResetToken(token string) (PasswordResetToken, error) {
+	var tok PasswordResetToken
+
+	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
+	defer cancel()
+
+	err := r.db.GetContext(ctx, &tok, "SELECT id, user_id, token, expiry FROM password_reset_token WHERE token = $1", token)
+	if err != nil {
+		return PasswordResetToken{}, r.handleError(err)
+	}
+
+	return tok, nil
+}
+
+func (r *UserRepository) DeleteAllPasswordResetTokensForUser(userId int) error {
+	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
+	defer cancel()
+
+	_, err := r.db.ExecContext(ctx, "DELETE FROM password_reset_token WHERE user_id = $1", userId)
+	return r.handleError(err)
+}
+
+func (r *UserRepository) DeletePasswordResetToken(token string) error {
+	ctx, cancel := newBackgroundContext(DefaultQueryTimeout)
+	defer cancel()
+
+	_, err := r.db.ExecContext(ctx, "DELETE FROM password_reset_token WHERE token = $1", token)
+	return r.handleError(err)
 }
