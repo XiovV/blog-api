6c5d0c8ba7a02ab46a0a8d2425aade55a7652767
XiovV <xiovv@tutanota.com>
2023-01-05 16:44:17 +0100

    added smtp environment variables

--- config/config.go
+++ config/config.go
@@ -5,10 +5,15 @@ import (
 )
 
 type Config struct {
-	PostgresDSN string `env:"POSTGRES_DSN" env-required:"true"`
-	Port        string `env:"PORT" env-default:"8080"`
-	Environment string `env:"ENV" env-default:"PRODUCTION"`
-	AESKey      string `env:"AES_KEY" env-required:"true"`
+	PostgresDSN  string `env:"POSTGRES_DSN" env-required:"true"`
+	Port         string `env:"PORT" env-default:"8080"`
+	Environment  string `env:"ENV" env-default:"PRODUCTION"`
+	AESKey       string `env:"AES_KEY" env-required:"true"`
+	SMTPHost     string `env:"SMTP_HOST" env-required:"true"`
+	SMTPPort     int    `env:"SMTP_PORT" env-required:"true"`
+	SMTPUsername string `env:"SMTP_USERNAME" env-required:"true"`
+	SMTPPassword string `env:"SMTP_PASSWORD" env-required:"true"`
+	SMTPSender   string `env:"SMTP_SENDER" env-required:"true"`
 }
 
 func New() (*Config, error) {
